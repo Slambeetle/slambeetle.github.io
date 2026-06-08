5dcf7585a773a56c570053e19aadb64ebbdccb53
Michael <mpinn@byandlarge.net>
2026-06-08 20:29:43 +1000

    Mobile support and tailwind CSS header / navigation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://slambeetle.github.io',
-  integrations: [mdx()]
+  integrations: [mdx()],
+   vite: {
+      plugins: [tailwindcss()],
+    },
 });
